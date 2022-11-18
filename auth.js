f35dffea77799bc6d65463546a768d013a2e25b6
j4v1r <99700245+j4v1r@users.noreply.github.com>
2022-11-18 10:04:33 -0600

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID = '173992933976-ku8apq4lvfsr988jjqav3fln4rk5aupb.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/google/callback",
+    passReqToCallback: true
+},
+    function (request, accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }
+));
+
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+require('./auth')
+
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
+const app = express();
+app.use(session({ secret: 'cats' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['email', 'profile'] })
+)
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/profile',
+        failureRedirect: '/auth/failure',
+    })
+);
+
+app.get('/auth/failure', (req, res) =>
+    res.send('Something went wrong')
+)
+
+app.get('/profile', isLoggedIn, (req, res) => {
+    res.render('perfil', { name: req.user.displayName, picture: req.user.picture, email: req.user.email, 
+    genero: req.user.gender, idioma: req.user.language, cumple: req.user.relationship})
+
+    //res.send(`Hello ${req.user.displayName} ${res.user.picture}`)
+
+})
+
+
+app.get('/protected', isLoggedIn, (req, res) => {
+
+    res.render('protegida')
+})
+
+app.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.session.destroy();
+        res.redirect('/')
+    })
+});
+
+app.listen(5000, () => console.log('Escuchando en el puerto 5000'))
